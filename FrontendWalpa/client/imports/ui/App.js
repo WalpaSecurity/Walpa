8cfe27b8e24629c4585cd4b6cf3085873d0ceca9
admin <admin@iMac3-2.local>
2018-01-16 12:20:50 +0100

    ajout librairies css bootstrap et fontawesome

--- FrontendWalpa/client/imports/ui/App.js
+++ FrontendWalpa/client/imports/ui/App.js
@@ -14,21 +14,21 @@ export default class App extends Component {
 
     renderTasks() {
         return this.getTasks().map((task) => (
-            <Task key={task._id} task={task} /> 
+            <Task key={task._id} task={task} />
     ));
     }
 
     render() {
         return (
             <div className="container">
-            <header>
-            <h1>Todo List</h1>
-        </header>
+              <header>
+                <h1>Todo List</h1>
+              </header>
 
-        <ul>
-        {this.renderTasks()}
-    </ul>
-        </div>
+              <ul>
+                {this.renderTasks()}
+              </ul>
+            </div>
     );
     }
 }
