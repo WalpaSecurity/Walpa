3bfc3954c86e060ad287e96d61cfc820db477234
shinigani <wilfried.atride@outlook.fr>
2018-01-17 11:19:34 +0100

    Modif main et ajout content

---
+++ FrontendWalpa/client/imports/ui/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+// App component - represents the whole app
+export default class Base extends Component {
+    render() {
+        return (
+            <div className='body'>
+            <p>LA PAGE BASE POPOPOPOPOPOPOPOPO</p>
+            </div>
+
+    );
+    }
+
+
+}
--- FrontendWalpa/client/main.js
+++ FrontendWalpa/client/main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute } from "react-router-dom";
 
 import App from './imports/ui/App.js';
 import Header from './imports/ui/Header.js';
+import Base from './imports/ui/Base.js';
 
 Meteor.startup(() => {
     render(<App />, document.getElementById('render-target'));
     render(<Header />, document.getElementById('header'));
+render(<Router>
+    <Route path="/connexion" component={Base}></Route>
+
+</Router> ,document.getElementById('app'));
 });
+
