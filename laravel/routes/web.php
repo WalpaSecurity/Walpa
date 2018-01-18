4872b893c8964439ac94c5778daacb1c40702464
Laure <catarino.laure@gmail.com>
2018-01-18 16:36:28 +0100

    Test

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/activity', 'ActivityReportController@index')->name('activity');
+Route::post('/activity', 'ActivityReportController@store');
+
+
+Route::get('/donation', 'DonationsController@index')->name('donation');
+
+Route::post('/donation', 'DonationsController@store');
