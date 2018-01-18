9835cdb4df3ccc4544289cac37952792fe8d9f34
patrice-pi <patrice.picollet@hotmail.fr>
2018-01-18 10:46:25 +0100

    ajout des routes, ajout pages dons/home, creation dossier css, responsive menu

--- FrontendWalpa/client/main.js
+++ FrontendWalpa/client/main.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import App from './imports/ui/App.js';
 import Header from './imports/ui/Header.js';
-import Base from './imports/ui/Base.js';
+import Base from './imports/ui/Content.js';
+import Home from './imports/ui/Home.js';
+import Dons from './imports/ui/Dons.js';
 
 Meteor.startup(() => {
-    render(<App />, document.getElementById('render-target'));
     render(<Header />, document.getElementById('header'));
-render(<Router>
-    <Route path="/connexion" component={Base}></Route>
-
-</Router> ,document.getElementById('app'));
+    render(<BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/faire-un-don" component={Dons}></Route>
+      </Switch>
+    </BrowserRouter>, document.getElementById('content'));
 });
 
+
+jQuery(function($){
+
+    // Ouverture du menu mobile et de l'overlay
+    $('body').on("click", ".navbar-toggler", function(){
+      $("#menu-mobile").animate({
+          opacity: 1,
+          left: "0px"
+      }, 300);
+      $('.overlay_mobile').fadeIn();
+      $('body').addClass("noscroll");
+    });
+    // Fermeture du menu mobile et de l'overlay
+    $('.overlay_mobile').click(function() {
+      $("#menu-mobile").animate({
+          opacity: 0,
+          left: "-80%"
+      }, 300);
+      $('.overlay_mobile').fadeOut();
+      $('body').removeClass("noscroll");
+    });
+});
