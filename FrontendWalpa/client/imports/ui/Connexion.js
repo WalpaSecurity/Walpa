2e220d56be31ae315d845fd2447bf4f4db18c0df
shinigani <wilfried.atride@outlook.fr>
2018-01-19 16:23:27 +0100

    Ajout du get dans connexion.js

--- FrontendWalpa/client/imports/ui/Connexion.js
+++ FrontendWalpa/client/imports/ui/Connexion.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
+import { HTTP } from 'meteor/http';
 
 // App component - represents the whole app
 export default class Connexion extends Component {
+    handleSubmitConnexion(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const email = ReactDOM.findDOMNode(this.refs.email).value.trim();
+        const password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+        console.log(email);
+        console.log(password);
+
+        HTTP.call('GET', 'https://swapi.co/api/people/1/', {}, (error, result) => {
+            if (!error) {
+            res=JSON.parse(result.content);
+                // debugger;
+            // console.log(result);
+            // console.log(JSON.parse(result));
+        }
+        HTTP.call('GET', 'https://swapi.co/api/people/1/', {}, (error, result) => {
+            if (!error) {
+            res=JSON.parse(result.content);
+            // debugger;
+            // console.log(result);
+            // console.log(JSON.parse(result));
+            }
+        }
+        // Clear form
+
+    }
+
+    handleSubmitInscription(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const emailinscrit = ReactDOM.findDOMNode(this.refs.emailinscrit).value.trim();
+        const name = ReactDOM.findDOMNode(this.refs.name).value.trim();
+        const surname = ReactDOM.findDOMNode(this.refs.surname).value.trim();
+        const passwordinscrit = ReactDOM.findDOMNode(this.refs.passwordinscrit).value.trim();
+        const passwordinscrit2 = ReactDOM.findDOMNode(this.refs.passwordinscrit2).value.trim();
+        const password = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        console.log(emailinscrit);
+
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
     render() {
         return (
                 <div id="home">
@@ -10,40 +56,42 @@ export default class Connexion extends Component {
                         <div className="row align-items-start justify-content-between">
                             <div className="col-5 connexion">
                                 <h2>Connexion</h2>
-                                <form>
+                                <form onSubmit={this.handleSubmitConnexion.bind(this)} >
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput">Adresse Mail</label>
-                                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="blablabla@blabla.fr"/>
+                                    <input type="text" className="form-control" id="formGroupExampleInput" ref="email" placeholder="blablabla@blabla.fr"/>
                                     </div>
                                     <div className="form-group">
                                     <label htmlFor="formGroupExampleInput2">Mot de passe</label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="00000000"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput2" ref="password" placeholder="00000000"/>
                                     </div>
+                                    <input type="submit" value="Connexion" />
                                 </form>
                             </div>
                             <div className="col-5">
                                 <h2>Inscription</h2>
-                                <form>
+                                <form onSubmit={this.handleSubmitInscription.bind(this)}>
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput">Adresse mail</label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="blablabla@blabla.fr"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput" ref="emailinscrit" placeholder="blablabla@blabla.fr"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput">Nom </label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="What's yo name son ?"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput" ref="name" placeholder="What's yo name son ?"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput">Prénom </label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="What's yo surname cunt ?"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput" ref="surname" placeholder="What's yo surname cunt ?"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput2">Mot de passe</label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Put here yo mothafucking password"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput2" ref="passwordinscrit" placeholder="Put here yo mothafucking password"/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="formGroupExampleInput2">Confirmation mot de passe</label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Put here yo mothafucking password again"/>
+                                        <input type="text" className="form-control" id="formGroupExampleInput2" ref="passwordinscrit2"placeholder="Put here yo mothafucking password again"/>
                                     </div>
+                                    <input type="submit" value="S'inscrire" />
                                 </form>
                             </div>
                         </div>
