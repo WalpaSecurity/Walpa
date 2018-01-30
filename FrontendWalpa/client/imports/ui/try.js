8df83dbf867562ad3e08d96641f2bfd3f5bc0860
Shinigani <wilwsadu69@hotmail.fr>
2018-01-30 16:58:57 +0100

    Ajout page test pour laravel 1.3

--- FrontendWalpa/client/imports/ui/try.js
+++ FrontendWalpa/client/imports/ui/try.js
@@ -7,6 +7,15 @@ export default class Try extends Component {
     handleSubmitTest(event){
         event.preventDefault();
         location.href = "192.168.1.16:5000";
+
+        HTTP.call('GET', '192.168.1.16:5000/try', {}, (error, result) =>{
+            if( !error ){
+            const res = JSON.parse(result);
+            //debugger;
+            console.log(res);
+            // console.log(JSON.parse(result));
+        }
+    });
     }
 
     render() {
@@ -14,7 +23,7 @@ export default class Try extends Component {
             <div className="col-12 col-md-6 col-lg-4 inscription">
                 <h2>Test</h2>
 
-            <form action="192.168.1.16:5000">
+            <form action="192.168.1.16:5000" onSubmit={this.handleSubmitTest.bind(this)}>
                 <input type="submit" value="C'est parti pour laravel" />
             </form>
             </div>
