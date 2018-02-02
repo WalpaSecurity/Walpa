3f2ab7e27fadc52a1c8f400fa9d09fc2f8e6c0bf
admin <admin@iMac3-2.local>
2018-02-02 14:58:03 +0100

    Cors 1.5

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -12,8 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
