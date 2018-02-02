fd34a22131ae0b07838327a6020771463ca94e13
Shinigani <wilwsadu69@hotmail.fr>
2018-02-02 11:47:55 +0100

    Test page connexion avec le back 1.8.2.g

--- FrontendWalpa/client/imports/ui/Connexion.js
+++ FrontendWalpa/client/imports/ui/Connexion.js
@@ -28,9 +28,18 @@ export default class Connexion extends Component {
             const token = res.token;
             HTTP.call('POST', 'http://192.168.1.16:5000/api/get-details', {
                 headers:{
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": "true",
+                    "Access-Control-Allow-Methods": 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                    "Access-Control-Allow-Headers": "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Content-Type, Authorization,Accept",
+                    'Content-Type' : "application/json",
                     'Authorization' : "Bearer " + token,
                     'Accept' : "application/json"
-                }
+
+                },
+                    data: {
+                        token: token
+                    }
                 },
                 (error, resultuser) => {
                     if (!error) {
