716f2072989a5c08d4ee314a7266903c7039d81c
Shinigani <wilwsadu69@hotmail.fr>
2018-02-02 10:46:26 +0100

    Test page connexion avec le back 1.7.7

--- FrontendWalpa/client/imports/ui/Connexion.js
+++ FrontendWalpa/client/imports/ui/Connexion.js
@@ -28,6 +28,7 @@ export default class Connexion extends Component {
             const token = res.token;
             HTTP.call('POST', 'http://192.168.1.16:5000/api/get-details', {
                 headers:{
+                    'Content-Type' : "application/json",
                     'Authorization' : "Bearer " + token,
                     'Accept' : "application/json"
 
