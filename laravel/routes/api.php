7838af97fec3cd47446c6b592b09fa647a902814
admin <admin@iMac3-2.local>
2018-02-05 11:39:46 +0100

    Add admin 1.4

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -60,7 +60,7 @@ Route::group(['middleware' => 'auth:api'], function () {
   //Partie ADMIN
 
 
-  Route::get('/adminU', 'AdminController@getUser');
+  Route::get('/admin', 'AdminController@getUser');
   Route::post('/admin/{id}', 'AdminController@changeID');
   Route::delete('/admin/{id}', 'AdminController@deleteAdmin');
 
