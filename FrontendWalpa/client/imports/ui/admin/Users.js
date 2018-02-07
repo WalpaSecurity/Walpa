b6545d4267ea75de9bb5447e7ef8b22008eea4ad
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-07 15:55:26 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/Users.js
+++ FrontendWalpa/client/imports/ui/admin/Users.js
@@ -9,6 +9,8 @@ export default class gestionUsers extends Component {
     handleListUsers(event){
       event.preventDefault();
 
+        console.log("toto");
+
       HTTP.call('GET', 'http://192.168.1.16:5000/api/admin', {
         headers:{
           "Access-Control-Allow-Headers": "Content-Type, Authorization,Accept , Access-Control-Allow-Headers",
