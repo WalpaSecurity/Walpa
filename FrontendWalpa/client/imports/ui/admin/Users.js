83003f0c5e0036621485d3e3bd6ddec743a01c38
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-07 16:03:46 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/Users.js
+++ FrontendWalpa/client/imports/ui/admin/Users.js
@@ -6,17 +6,31 @@ const token = localStorage.getItem('token');
 // App component - represents the whole app
 export default class gestionUsers extends Component {
 
-    handleListUsers(){
+    handleListUsers(e){
+      e.preventDefault();
       console.log("toto");
-
-      
+      HTTP.call('GET', 'http://192.168.1.16:5000/api/admin', {
+        headers:{
+          "Access-Control-Allow-Headers": "Content-Type, Authorization,Accept , Access-Control-Allow-Headers",
+          'Content-Type' : "application/json",
+          'Authorization' : "Bearer " + token,
+          'Accept' : "application/json"
+        }
+      }, (error, result) => {
+        if (!error) {
+          const users = JSON.parse(result.content);
+          console.log(result);
+          console.log(users.data);
+        }
+      });
     }
 
 
     render() {
         return (
-          <div className="adminContent gestionUsers" onLoad={this.handleListUsers.bind(this)}>
+          <div className="adminContent gestionUsers">
             <h1>Liste des utilisateurs</h1>
+            <a href="/" className="btn btn-primary" onClick={this.handleListUsers.bind(this)}></a>
             <table className="table table-striped">
               <thead>
                 <tr>
