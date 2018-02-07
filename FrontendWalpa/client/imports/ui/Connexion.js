175c682e4b0ef036c204a6a9deaabf25e7c75bec
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-07 11:40:11 +0100

    test connect admin

--- FrontendWalpa/client/imports/ui/Connexion.js
+++ FrontendWalpa/client/imports/ui/Connexion.js
@@ -46,7 +46,7 @@ export default class Connexion extends Component {
                         console.log(resultuser);
                         const resuser = JSON.parse(resultuser.content);
                         console.log(resuser);
-                        console.log(resuser.admin);
+                        console.log(resuser.content);
                         return false;
                         document.location.reload(true);
                         this.props.history.push('/profile');
