616a1c139e331f81849b80078e880c9dc3526883
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-07 11:30:50 +0100

    test connect admin

--- FrontendWalpa/client/imports/ui/Connexion.js
+++ FrontendWalpa/client/imports/ui/Connexion.js
@@ -42,19 +42,15 @@ export default class Connexion extends Component {
                     }
                 },
                 (error, resultuser) => {
-                  if (!error) {
-                      console.log(resultuser);
-                      const resuser = JSON.parse(resultuser.content);
-                      console.log(resuser);
-                      console.log(resuser.admin);
-                      document.location.reload(true);
-                      return false;
-                      if(resuser.admin == 1){
-                        this.props.history.push('/admin');
-                      } else {
-                        this.props.history.push('/profile');
-                      }
-                  }
+                    if (!error) {
+                        console.log(resultuser);
+                        const resuser = JSON.parse(resultuser.content);
+                        console.log(resuser);
+                        console.log(resuser.user);
+                        document.location.reload(true);
+                        return false;
+                        // this.props.history.push('/profile');
+                    }
             });
 
         }
