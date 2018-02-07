de61ad0ebfb46fd847ca127dfb8717b45ec311e6
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-07 16:59:23 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/AddAdmin.js
+++ FrontendWalpa/client/imports/ui/admin/AddAdmin.js
@@ -3,26 +3,46 @@
 
 // App component - represents the whole app
 export default class AjouterAdmin extends Component {
+
+    handleAddAdmin(event) {
+      event.preventDefault();
+
+      // Find the text field via the React ref
+      const emailNewAdmin = ReactDOM.findDOMNode(this.refs.emailNewAdmin).value.trim();
+      const nameNewAdmin = ReactDOM.findDOMNode(this.refs.emailNewAdmin).value.trim();
+      const passwordNewAdmin = ReactDOM.findDOMNode(this.refs.passwordNewAdmin).value.trim();
+      //const password = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+      HTTP.call('POST', 'http://192.168.1.16:5000/api/register', {
+        data: {
+          email: emailNewAdmin,
+          name: nameNewAdmin,
+          password: passwordNewAdmin,
+          admin: 1,
+        }
+      }, (error, result) => {
+        if (!error) {
+          console.log(result);
+        }
+      });
+    }
+
     render() {
         return (
           <div className="adminContent add-admin">
             <h1>Ajouter un administrateur</h1>
-            <form>
+            <form onSubmit={this.handleAddAdmin.bind(this)}>
                 <div className="form-group">
                     <label htmlFor="email">Adresse mail</label>
-                    <input type="text" className="form-control" id="email" ref="emailinscrit" placeholder="Entrez une adresse e-mail"/>
+                    <input type="text" className="form-control" id="email" ref="emailNewAdmin" placeholder="Entrez une adresse e-mail"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="nom">Nom </label>
-                    <input type="text" className="form-control" id="nom" ref="name" placeholder="Entrez un nom"/>
+                    <input type="text" className="form-control" id="nom" ref="nameNewAdmin" placeholder="Entrez un nom"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password1">Mot de passe</label>
-                    <input type="password" className="form-control" id="password1" ref="passwordinscrit" placeholder="Saisissez un mot de passe"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password2">Confirmation mot de passe</label>
-                    <input type="password" className="form-control" id="password2" ref="passwordinscrit2" placeholder="Confirmez le mot de passe"/>
+                    <input type="password" className="form-control" id="password1" ref="passwordNewAdmin" placeholder="Saisissez un mot de passe"/>
                 </div>
                 <input className="btn btn-primary" type="submit" value="Ajouter un administrateur"/>
             </form>
