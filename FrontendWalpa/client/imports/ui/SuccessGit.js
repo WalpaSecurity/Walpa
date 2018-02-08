225c91e6889e02ec6bec31af3ef5fbb15ddbb17f
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-08 11:58:16 +0100

    modif panel admin

---
+++ FrontendWalpa/client/imports/ui/SuccessGit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { HTTP } from 'meteor/http';
+import { Session } from 'meteor/session';
+
+// App component - represents the whole app
+export default class SuccessGit extends Component {
+
+    render() {
+        return (
+          <div>toto</div>
+    );
+    }
+}
