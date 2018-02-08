d5ccd290b2cd2e9dccfc07880add05fcf7caf68d
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-08 15:55:03 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/Users.js
+++ FrontendWalpa/client/imports/ui/admin/Users.js
@@ -54,10 +54,12 @@ export default class gestionUsers extends Component {
       console.log("on supprime un admin");
       var id = $(this).closest('tr').data('id');
       var id2 = $(this).closest('tr');
-      var id3 = $(this).parent().parent().data('id');
+      var id3 = $(this).parent('tr');
+      var id4 = $(this);
       console.log(id);
       console.log(id2);
       console.log(id3);
+      console.log(id4);
 
       HTTP.call('DELETE', 'http://192.168.1.16:5000/api/admin', {
         headers:{
