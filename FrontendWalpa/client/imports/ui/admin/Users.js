c8584d845c4be0e8c7494e63c472b3daa1454f0c
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-08 10:25:28 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/Users.js
+++ FrontendWalpa/client/imports/ui/admin/Users.js
@@ -47,10 +47,30 @@ export default class gestionUsers extends Component {
         }
       });
     }
+    handleDeleteAdmine){
+      e.preventDefault();
+      console.log("on supprime un user");
+      console.log($(this).closest('tr').data('id'));
+
+      HTTP.call('DELETE', 'http://192.168.1.16:5000/api/admin', {
+        headers:{
+          "Access-Control-Allow-Headers": "Content-Type, Authorization,Accept , Access-Control-Allow-Headers",
+          'Content-Type' : "application/json",
+          'Authorization' : "Bearer " + token,
+          'Accept' : "application/json"
+        }
+      }, (error, result) => {
+        if (!error) {
+          console.log("good");
+        } else {
+          console.log("pas good");
+        }
+      });
+    }
 
     render() {
         return (
-          <div className="adminContent gestionUsers"onLoad={this.handleGetListUsers.bind(this)}>
+          <div className="adminContent gestionUsers" onLoad={this.handleGetListUsers.bind(this)}>
             <h1>Liste des utilisateurs</h1>
             <a href="/" className="btn btn-primary">Rafraichir</a>
             <table className="table table-striped" id="listUser">
