e9f16351f2761e0d984b20a2d9b80e5cb6825540
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-08 12:06:42 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/SuccessGit.js
+++ FrontendWalpa/client/imports/ui/SuccessGit.js
@@ -6,6 +6,22 @@ import { Session } from 'meteor/session';
 // App component - represents the whole app
 export default class SuccessGit extends Component {
 
+  handleGitConnect(){
+
+    HTTP.call('GET', 'http://192.168.1.16:5000/auth/github/callback', {
+
+    }, (error, result) => {
+      if (!error) {
+        const users = JSON.parse(result.content);
+        console.log(result);
+        console.log(users.data);
+        console.log("good");
+      } else {
+        console.log("pas good");
+      }
+    });
+  }
+
     render() {
         return (
           <div>toto</div>
