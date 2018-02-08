62ba8fcde9451abc08b1e6306ebd2df0e079eace
Shinigani <wilwsadu69@hotmail.fr>
2018-02-08 10:19:45 +0100

    Gestion data repo 1.2.10

--- FrontendWalpa/client/imports/ui/Profile.js
+++ FrontendWalpa/client/imports/ui/Profile.js
@@ -66,12 +66,7 @@ export default class Profile extends Component {
         for (var i = 0; i< TableauRepo.length; i++ )
         {
             console.log(TableauRepo[i].file_name);
-            renderedHtml +=<tr>
-                <td>1</td>
-                <td>baz</td>
-                <td>foo</td>
-                <td>bar</td>
-            </tr>;
+            renderedHtml +=<tr></tr>;
         }
 
         return renderedHtml;
