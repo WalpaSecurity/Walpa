0b327804f0110ce3b6129d593b8bb98854b1517a
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-08 14:48:13 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/SuccessGit.js
+++ FrontendWalpa/client/imports/ui/SuccessGit.js
@@ -6,41 +6,31 @@ import { Session } from 'meteor/session';
 // App component - represents the whole app
 export default class SuccessGit extends Component {
 
-  handleGitConnect(){
+    handleGitConnect(){
 
-    HTTP.call('GET', 'http://192.168.1.16:5000/auth/github/callback', {
+      HTTP.call('GET', 'http://192.168.1.16:5000/auth/github/callback', {
 
-    }, (error, result) => {
-      if (!error) {
-        const users = JSON.parse(result.content);
-        console.log(result);
-        console.log(users.data);
-        console.log("good");
-      } else {
-        console.log("pas good");
-      }
+      }, (error, result) => {
+        if (!error) {
+          const users = JSON.parse(result.content);
+          console.log(result);
+          console.log(users.data);
+          console.log("good");
+        } else {
+          console.log("pas good");
+        }
+      });
+    }
+    $(window).load(function(){
+      setTimeout(function () {
+          handleGitConnect();
+      }, 3000);
     });
-  }
-
-  handleScriptCreate() {
-    console.log("function1");
-  }
-
-  handleScriptError() {
-    console.log("function2");
-  }
-
-  handleScriptLoad() {
-    console.log("function3");
-  }
 
     render() {
         return (
           <div>
-
-          toto
-            <div onLoad={this.handleGitConnect.bind(this)} className="btn btn-success">bonjour</div>
-            <script onLoad={this.handleScriptLoad.bind(this)} />
+              toto
           </div>
     );
     }
