ac1b0a0c071aba99d2ddfb2bd14c3905973a4270
Laure <catarino.laure@gmail.com>
2018-02-08 14:55:56 +0100

    Oauth GIt

--- laravel/app/Http/Controllers/Auth/OauthController.php
+++ laravel/app/Http/Controllers/Auth/OauthController.php
@@ -40,30 +40,12 @@ class OauthController extends Controller
 
       Auth::login($authUser, true);
 
-    /*  if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+      $user = Auth::user();
+       $success['token'] =  $user->createToken('MyApp')->accessToken;
 
-      }else{
+      dd($success['token']);
 
-      }*/
-
-      try {
-          $user = Socialite::driver('github')->user();
-      } catch (Exception $e) {
-          return Redirect::to('auth/github');
-      }
-      $authUser = $this->findOrCreateUser($user);
-
-      if(Auth::login($authUser, true)){
-        $user = Auth::user();
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        dd($success['token']);
-        return response()->json(['success' => true, 'token' => $user->createToken('MyApp')->accessToken]);
-      }else{
-        return response()->json(['success' => false, 'err' => "Error..."]);
-      }
-
-
-    //  return Redirect::to('home');
+      return Redirect::to('home');
   }
 
   /**
@@ -89,81 +71,3 @@ class OauthController extends Controller
 
   }
 }
-
-/*
-
-
-if(Auth::login($authUser, true)){
-  $user = Auth::user();
-  $success['token'] =  $user->createToken('MyApp')->accessToken;
-  $changeUser = DB::table('users')
-         ->where('id', Auth::user()->id)
-         ->update(['remember_token' => $user->createToken('MyApp')->accessToken]);
-  return response()->json(['success' => true, 'token' => $user->createToken('MyApp')->accessToken]);
-}else{
-  return response()->json(['success' => false, 'err' => "Error..."]);
-}
-
-
-namespace App\Http\Controllers\Auth;
-
-
-use App\User;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Auth;
-use Redirect;
-use Socialite;
-
-class OauthController extends Controller
-{
-
-  public function redirectToProvider()
-  {
-      return Socialite::driver('github')->redirect();
-  }
-
-
-  public function handleProviderCallback()
-  {
-      //return response()->json(['toto' => "totoot"]);
-
-      try {
-          $user = Socialite::driver('github')->user();
-      } catch (Exception $e) {
-          return Redirect::to('auth/github');
-      }
-      $authUser = $this->findOrCreateUser($user);
-      dd(Auth::login($authUser, true));
-
-      if(Auth::login($authUser, true)){
-        $user = Auth::user();
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        return response()->json(['success' => true, 'token' => $user->createToken('MyApp')->accessToken]);
-      }else{
-        return response()->json(['success' => false, 'err' => "Error..."]);
-      }
-  }
-
-
-  private function findOrCreateUser($githubUser)
-  {
-
-      if ($authUser = User::where('github_id', $githubUser->id)->first()) {
-          return $authUser;
-      }
-     return User::create([
-          'name' => $githubUser->name,
-          'email' => $githubUser->email,
-          'password' => "",
-          'admin' => false,
-          'github_id' => $githubUser->id,
-      ]);
-
-  }
-}
-
-*/
