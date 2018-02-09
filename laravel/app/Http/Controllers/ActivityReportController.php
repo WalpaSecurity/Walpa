e7517eab06a57be1d59923f53ac33449757f6c7e
Laure <catarino.laure@gmail.com>
2018-02-09 08:40:30 +0100

    Queues

--- laravel/app/Http/Controllers/ActivityReportController.php
+++ laravel/app/Http/Controllers/ActivityReportController.php
@@ -90,29 +90,29 @@ class ActivityReportController extends Controller
 
         //COMMENCEMENT DE L'ANALYSE DU FICHIER PHP :
         //Clonage du fichier git
-        shell_exec("git clone ". $this->url ." /var/www/laravel/public/temp/".$this->number);
+        shell_exec("git clone ". $request->url ." /var/www/laravel/public/temp/".$number);
 
         //PHP CODE SNIFFER : that tokenizes PHP, JavaScript and CSS files to detect violations of a defined coding standard
-        $PHPCODESNIFFER = shell_exec("phpcs --standard=LaravelCodeSniffer/Standards/Laravel/ /var/www/laravel/public/temp/".$this->number);
+        $PHPCODESNIFFER = shell_exec("phpcs --standard=LaravelCodeSniffer/Standards/Laravel/ /var/www/laravel/public/temp/".$number);
 
         //PHP LOC : is a tool for quickly measuring the size and analyzing the structure of a PHP project
-        $PHPLOC = shell_exec("phploc /var/www/laravel/public/temp/".$this->number);
+        $PHPLOC = shell_exec("phploc /var/www/laravel/public/temp/".$number);
 
         //PHP Copy/Paste Detector : is a Copy/Paste Detector (CPD) for PHP code.
-        $PHPCPD = shell_exec("phpcpd /var/www/laravel/public/temp/".$this->number);
+        $PHPCPD = shell_exec("phpcpd /var/www/laravel/public/temp/".$number);
 
         //Phortress : static code analyser for potential vulnerabilities
         //shell_exec("sudo apt-get install phpunit");
         //    $PHortress = shell_exec("phpunit /var/www/html/public/temp"); //MARCHE PAS
 
         //PHP Coding Standards Fixer : The PHP Coding Standards Fixer (PHP CS Fixer) tool fixes your code to follow standards; whether you want to follow PHP coding standards as defined in the PSR-1, PSR-2$
-        //$PHPCoding = shell_exec("php-cs-fixer fix /var/www/laravel/public/temp/".$this->number);
+        //$PHPCoding = shell_exec("php-cs-fixer fix /var/www/laravel/public/temp/".$number);
         echo("_ fixer");
         //  PHP Metrics :
         //  shell_exec("php ./vendor/bin/phpmetrics --report-html=myreport /var/www/html/public/temp/".$number);
         //  shell_exec("cp -R /var/www/html/myreport /var/www/html/public/temp/metrics_". $number);
-       //        rmdir("/var/www/laravel/public/temp/".$this->number);
-        shell_exec("rm -rf /var/www/laravel/public/temp/".$this->number);
+       //        rmdir("/var/www/laravel/public/temp/".$number);
+        shell_exec("rm -rf /var/www/laravel/public/temp/".$number);
         //  Rassemblement de tous les résultats
         $str_result = "-------------------------------------------------------------------------------- \n D  tection des violations dans les fichiers PHP, JS et CSS : \n\n\n " . $PHPCODESNIFFER ;
         $str_result .= "\n-------------------------------------------------------------------------------- \n\n Analyse de la taille et la structure du projet PHP : \n\n\n" . $PHPLOC ;
@@ -125,10 +125,10 @@ class ActivityReportController extends Controller
         //  shell_exec("rm /var/www/html/public/temp/result.txt");
         //Cr  ation du fichier texte qui va contenir le r  sultat
         echo ("AVANT creation file");
-        shell_exec("touch /var/www/laravel/public/temp/". $this->name_file .".txt");
-        //file_put_contents("/var/www/laravel/public/temp/". $this->name_file . ".txt");
+        shell_exec("touch /var/www/laravel/public/temp/". $name_file .".txt");
+        //file_put_contents("/var/www/laravel/public/temp/". $name_file . ".txt");
         echo("_ creation file");
-        $file = '/var/www/laravel/public/temp/'. $this->name_file .'.txt';
+        $file = '/var/www/laravel/public/temp/'. $name_file .'.txt';
         echo("_ dollard file");
 
         // Ouvre un fichier pour lire un contenu existant
@@ -145,9 +145,9 @@ class ActivityReportController extends Controller
 
         //Analyse termin  e
         $activityReport = DB::table('activityReport')
-              ->where('user_id', $this->id)
-              ->where('url', $this->url)
-              ->where('file_name', $this->name_file)
+              ->where('user_id', $id)
+              ->where('url', $request->url)
+              ->where('file_name', $name_file)
               ->update(['statut' => "TERMINEE"]);
 
 
