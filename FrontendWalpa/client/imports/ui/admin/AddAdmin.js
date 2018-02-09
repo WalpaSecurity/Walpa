de6388cb82374ec04ea2cdbe0029714fba289a0a
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-09 09:43:09 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/admin/AddAdmin.js
+++ FrontendWalpa/client/imports/ui/admin/AddAdmin.js
@@ -19,32 +19,40 @@ export default class AjouterAdmin extends Component {
       if(emailNewAdmin != ""){
         if(nameNewAdmin != ""){
           if(passwordNewAdmin != ""){
-            HTTP.call('POST', 'http://192.168.1.16:5000/api/admin', {
-              headers:{
-                  "Access-Control-Allow-Headers": "Content-Type, Authorization,Accept , Access-Control-Allow-Headers",
-                  'Content-Type' : "application/json",
-                  'Authorization' : "Bearer " + token,
-                  'Accept' : "application/json"
+            if(passwordNewAdmin.length >= 6){
+              HTTP.call('POST', 'http://192.168.1.16:5000/api/admin', {
+                headers:{
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization,Accept , Access-Control-Allow-Headers",
+                    'Content-Type' : "application/json",
+                    'Authorization' : "Bearer " + token,
+                    'Accept' : "application/json"
 
-              },
-              data: {
-                email: emailNewAdmin,
-                name: nameNewAdmin,
-                password: passwordNewAdmin,
-                admin: 1,
-              }
-            }, (error, result) => {
-              if (!error) {
-                console.log(result);
-                $('#erreurAjoutAdmin').removeClass('alert-danger');
-                $('#erreurAjoutAdmin').addClass('alert-success');
-                $('#erreurAjoutAdmin').html("Le compte "+emailNewAdmin+" à bien été crée. Vous pouvez desormais vous connecter avec ce compte !");
-                $('#erreurAjoutAdmin').show();
-              } else {
-                $('#erreurAjoutAdmin').html("Une des informations n'est pas valide !");
-                $('#erreurAjoutAdmin').show();
-              }
-            });
+                },
+                data: {
+                  email: emailNewAdmin,
+                  name: nameNewAdmin,
+                  password: passwordNewAdmin,
+                  admin: 1,
+                }
+              }, (error, result) => {
+                if (!error) {
+                  console.log(result);
+                  $('#erreurAjoutAdmin').removeClass('alert-danger');
+                  $('#erreurAjoutAdmin').addClass('alert-success');
+                  $('#erreurAjoutAdmin').html("Le compte "+emailNewAdmin+" à bien été crée. Vous pouvez desormais vous connecter avec ce compte !");
+                  $('#erreurAjoutAdmin').show();
+                } else {
+                  $('#erreurAjoutAdmin').html("Une des informations n'est pas valide !");
+                  $('#erreurAjoutAdmin').show();
+                }
+              });
+            } else {
+              $('#erreurAjoutAdmin').html("Votre mot de passe doit contenir au moins 6 caractères !");
+              $('#erreurAjoutAdmin').show();
+              setTimeout(function () {
+                  $('#erreurAjoutAdmin').hide();
+              }, 3000);
+            }
           } else {
             $('#erreurAjoutAdmin').html("Vous devez taper un mot de passe !");
             $('#erreurAjoutAdmin').show();
