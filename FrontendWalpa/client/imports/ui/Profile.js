7dd10667fc372386eeb1e5ab1bcb002df5dce773
patrice-pi <patrice.picollet@hotmail.fr>
2018-02-09 12:26:50 +0100

    modif panel admin

--- FrontendWalpa/client/imports/ui/Profile.js
+++ FrontendWalpa/client/imports/ui/Profile.js
@@ -24,7 +24,7 @@ export default class Profile extends Component {
       },
       (error, result) => {
         if (!error) {
-          $('#modalContent').html(result.content);
+          $('#modalContent').text(data);
         }
       });
     }
